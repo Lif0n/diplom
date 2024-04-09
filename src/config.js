c54702e3dffc378c9024864cdbde19f083dfd6c7
Силов Илья <iliya.silov@gmail.com>
2024-04-09 15:09:52 +0400

    asd

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 const isProduction = process.env.NODE_ENV === 'production';
+const apiUrl = process.env.API_URL || 'localhost:8080';
 
 /**
  * Настройки сервисов
@@ -14,7 +15,7 @@ const config = {
     }
   },
   api: {
-    baseUrl: ''
+    baseUrl: apiUrl
   }
 }
 
