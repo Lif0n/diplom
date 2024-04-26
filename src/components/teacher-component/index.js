0e7ca7a347179017c9a835990ae9b339091fe809
Lif0n <t4sty.c4ke@gmail.com>
2024-04-26 18:01:22 +0400

    надо будет пофиксить метод добавления предмета на табе группы

--- src/components/teacher-component/index.js
+++ src/components/teacher-component/index.js
@@ -1,5 +1,6 @@
-import { memo, useMemo } from 'react'
+import { memo, useMemo, useState } from 'react'
 import { cn as bem } from '@bem-react/classname';
+import Spinner from '../spinner/index'
 import { Input, Card, Button } from 'antd';
 import './style.css'
 import Wrapper from '../wrapper';
@@ -18,15 +19,35 @@ function TeacherComponent({ arr }) {
     return groups;
   }, [arr])
 
+  const contentList = useMemo(() => {
+    const contentList = {};
+    groups.forEach(group => {
+      const subjs = arr.filter(groupTeacher => groupTeacher.group.id === group.key);
+      subjs.forEach(subj => {
+        console.log(subj);
+        contentList[group.key] = <h4>{subj.subject.name}</h4>
+      });
+    })
+    return contentList;
+  }, [arr, groups])
+
+  const [activeTab, setActiveTab] = useState(groups[0].id);
+
+  const onTabChange = (key) => {
+    setActiveTab(key);
+  }
+
+  console.log(contentList);
+
   return (
     <Wrapper>
-      <Input defaultValue={arr[0].teacher.surname} style={{maxWidth:'800px'}} className='teacherSurname' size='large' addonBefore='Фамилия' />
-      <Input defaultValue={arr[0].teacher.name} style={{maxWidth:'800px'}} className='teacherName' size='large' addonBefore='Имя' />
-      <Input defaultValue={arr[0].teacher.patronymic} style={{maxWidth:'800px'}} className='teacherPatronymic' size='large' addonBefore='Отчество' />
-      <Card title='Связи' style={{maxWidth:'800px'}}
+      <Input defaultValue={arr[0].teacher.surname} style={{ maxWidth: '800px' }} className='teacherSurname' size='large' addonBefore='Фамилия' />
+      <Input defaultValue={arr[0].teacher.name} style={{ maxWidth: '800px' }} className='teacherName' size='large' addonBefore='Имя' />
+      <Input defaultValue={arr[0].teacher.patronymic} style={{ maxWidth: '800px' }} className='teacherPatronymic' size='large' addonBefore='Отчество' />
+      <Card title='Связи' style={{ maxWidth: '800px' }}
         extra={<Button>Добавить группу к преподавателю</Button>}
-        tabList={groups}>
-
+        tabList={groups} onTabChange={onTabChange}>
+        {contentList[activeTab]}
       </Card>
     </Wrapper>
   );
