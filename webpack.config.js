14ff9e63b8d4a9c7e24ea81ef6c74804e87281b2
Lif0n <t4sty.c4ke@gmail.com>
2024-04-30 01:27:19 +0400

    aboba

--- webpack.config.js
+++ webpack.config.js
@@ -62,7 +62,7 @@ let config = {
     new webpack.DefinePlugin({
       'process.env': {
         NODE_ENV: JSON.stringify(process.env.NODE_ENV),
-        API_URL: JSON.stringify(process.env.NODE_ENV === 'production' ? 'http://hnt8.ru:1149':'http://hnt8.ru:1149')
+        API_URL: JSON.stringify(process.env.NODE_ENV === 'production' ? 'https://raspisanie.hnt8.ru':'https://raspisanie.hnt8.ru')
       },
     }),
   ],
