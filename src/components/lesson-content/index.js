53bdd65201338c001b543b63b113602fe0bec1c3
Lif0n <t4sty.c4ke@gmail.com>
2024-05-06 00:45:04 +0400

    background fix

--- src/components/lesson-content/index.js
+++ src/components/lesson-content/index.js
@@ -18,14 +18,17 @@ function LessonContent({ item, onItemClick }) {
     return errors;
   }
 
+  if(item.id == 602 || item.id == 51 || item.id == 249 ){
+    console.log(item);
+  }
 
   return (
     <Tooltip title={item.errors? error : ''} color='orange'>
       <div className='LessonContent' onClick={callbacks.onClick}>
         <h6 className='h-50'>{item.subject?.name}</h6>
         <div className='row h-50'>
-          {item.audience && <h6 className='col'>{item.audience?.number}</h6>}
-          <h6 className='col'>{item?.teachers[0]?.surname} {item?.teachers[0]?.name}. {item?.teachers[0]?.patronymic}.</h6>
+          {item.audience && <h6 className='col m-0'>{item.audience?.number}</h6>}
+          <h6 className='col m-0'>{item?.teachers[0]?.surname} {item?.teachers[0]?.name}. {item?.teachers[0]?.patronymic}.</h6>
         </div>
       </div>
     </Tooltip>
--- src/components/lesson/index.js
+++ src/components/lesson/index.js
@@ -9,31 +9,31 @@ function Lesson({ items, weekday, group, onItemClick, lessonNumber }) {
 
 
   const callbacks = {
-    onEmptyClick: (e) => onItemClick({lessonNumber: lessonNumber, weekday: weekday, group: group}),
+    onEmptyClick: (e) => onItemClick({ lessonNumber: lessonNumber, weekday: weekday, group: group }),
   }
 
   return (
     <div className={'row border m-1 p-0 ' + cn()}>
-      {items.length === 1 && items[0].weekNumber === 0 && <div className={items[0].errors ? 'error' : ''}><LessonContent item={items[0]} onItemClick={onItemClick}/></div>}
+      {items.length === 1 && items[0].weekNumber === 0 && <div className={(items[0].errors ? 'error' : '')}><LessonContent item={items[0]} onItemClick={onItemClick} /></div>}
       {items.length === 2 &&
         <>
-          <div className={'border half'+items[0].errors ? ' error' : ''}>
-            <LessonContent item={items[0]} onItemClick={(item) => onItemClick(item, true)}/>
+          <div className={'border half' + (items[0].errors ? ' error' : '')}>
+            <LessonContent item={items[0]} onItemClick={(item) => onItemClick(item, true)} />
           </div>
-          <div className={'border half'+items[1].errors ? ' error' : ''}>
-            <LessonContent item={items[1]} onItemClick={(item) => onItemClick(item, true)}/>
+          <div className={'border half' + (items[1].errors ? ' error' : '')}>
+            <LessonContent item={items[1]} onItemClick={(item) => onItemClick(item, true)} />
           </div>
         </>
       }
       {items.length === 1 && items[0].weekNumber === 1 &&
         <>
-          <div className='border half' onClick={(e) => onItemClick({lessonNumber: lessonNumber, weekday: weekday, group: group, weekNumber: 0}, true)}></div>
-          <div className={'border half'+items[0].errors ? ' error' : ''}>
-            <LessonContent item={items[0]} onItemClick={(item) => onItemClick(item, true)}/>
+          <div className='border half' onClick={(e) => onItemClick({ lessonNumber: lessonNumber, weekday: weekday, group: group, weekNumber: 0 }, true)}></div>
+          <div className={'border half' + (items[0].errors ? ' error' : '')}>
+            <LessonContent item={items[0]} onItemClick={(item) => onItemClick(item, true)} />
           </div>
         </>
       }
-      {items.length === 0 && <div onClick={(e) => onItemClick({lessonNumber: lessonNumber, weekday: weekday, group: group})}></div>}
+      {items.length === 0 && <div onClick={(e) => onItemClick({ lessonNumber: lessonNumber, weekday: weekday, group: group })}></div>}
     </div>
   );
 }
