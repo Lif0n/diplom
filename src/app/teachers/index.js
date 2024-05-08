1bb3e8b41c565e7012fc5da8ede84e0aa06bf939
Lif0n <t4sty.c4ke@gmail.com>
2024-05-08 12:58:01 +0400

    change search field

--- src/app/teachers/index.js
+++ src/app/teachers/index.js
@@ -1,15 +1,17 @@
-import { memo, useMemo } from "react";
+import { memo, useCallback, useMemo } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import useInit from "../../hooks/use-init";
 import groupTeachersActions from '../../store/group-teachers/actions';
 import PageLayout from "../../components/page-layout";
 import Header from "../../components/header";
 import logo from '../../img/logo.png';
-import { Collapse, Input } from "antd";
+import {SearchOutlined} from '@ant-design/icons';
+import { Collapse } from "antd";
 import LessonPlanLayout from "../../components/lesson-plan-layout";
 import uniqueValues from "../../utils/unique-values";
 import TeacherComponent from "../../components/teacher-component";
 import Wrapper from "../../components/wrapper";
+import InputSearch from "../../components/input-search";
 
 function Teachers() {
 
@@ -23,24 +25,31 @@ function Teachers() {
     groupTeachers: state.groupTeachers.list,
   }))
 
-  const teachers = useMemo(() => {
+  const teachers = useMemo((arr) => {
     const teachers = [];
-    uniqueValues(select.groupTeachers, 'teacher').forEach(teacher => {
+    uniqueValues(arr, 'teacher').forEach(teacher => {
       teachers.push({
         key: teacher.id, label: `${teacher.surname} ${teacher.name} ${teacher.patronymic}`,
-        children: <TeacherComponent arr={select.groupTeachers.filter(gt => gt.teacher.id === teacher.id)} />
+        children: <TeacherComponent arr={arr.filter(gt => gt.teacher.id === teacher.id)} />
       })
     });
     return teachers;
   }, [select.groupTeachers])
+
+  const callbacks = {
+    onSearch: useCallback()
+  }
 
   return (
     <PageLayout>
       <Header logo={logo} selected={'teachers'} />
       <LessonPlanLayout>
         <Wrapper>
-          <Input className="mt-2" placeholder="Поиск"/>
-          <Collapse items={teachers} />
+          <InputSearch
+          placeholder='Поиск'
+          prefix={<SearchOutlined/>}
+          size='large'/>
+          <Collapse items={teachers(select.groupTeachers)} />
         </Wrapper>
       </LessonPlanLayout>
     </PageLayout>
--- src/components/input-search/index.js
+++ src/components/input-search/index.js
@@ -1,8 +1,32 @@
 import { Input } from "antd";
-import { memo } from "react";
+import { memo, useState, useCallback, useLayoutEffect } from "react";
+import debounce from "lodash.debounce";
 
-function InputSearch() {
+function InputSearch(props) {
+  // Внутренний стейт для быстрого отображения ввода
+  const [value, setValue] = useState(props.value);
 
+  const onChangeDebounce = useCallback(
+    debounce(value => props.onChange(value, props.name), 600),
+    [props.onChange, props.name]
+  );
+
+  // Обработчик изменений в поле
+  const onChange = (event) => {
+    setValue(event.target.value);
+    onChangeDebounce(event.target.value);
+  };
+
+  useLayoutEffect(() => setValue(props.value), [props.value]);
+
+  return (
+    <Input
+      size={props.size}
+      placeholder={props.placeholder}
+      prefix={props.prefix} 
+      value={value}
+      onChange={onChange}/>
+  );
 }
 
 export default memo(InputSearch);
