3c72d59dc612c9337381d1068480b8b0f6f2b13b
Lif0n <t4sty.c4ke@gmail.com>
2024-05-23 03:48:55 +0400

    new alert

--- src/app/lesson-modal/index.js
+++ src/app/lesson-modal/index.js
@@ -11,6 +11,8 @@ import audiencesActions from '../../store/audiences/actions';
 import Spinner from "../../components/spinner";
 import { Radio, Flex, Button } from "antd";
 import LessonSelect from "../../components/lesson-select";
+import { ToastContainer, toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 function LessonModal({ lessonPlan, notChangeWeek }) {
 
@@ -44,27 +46,27 @@ function LessonModal({ lessonPlan, notChangeWeek }) {
     onAccept: useCallback(async () => {
 
       if (!teachers[0]) {
-        alert('Не выбран основной преподаватель');
+        toast.error('Не выбран основной преподаватель');
         return;
       }
       if (!lesson.subject) {
-        alert('Не выбран предмет');
+        toast.error('Не выбран предмет');
         return;
       }
 
       if (teachers[0] == teachers[1]) {
-        alert('Основной и запасной преподаватель не могут быть одинаковыми');
-        return;   
+        toast.error('Основной и запасной преподаватель не могут быть одинаковыми');
+        return;
       }
 
-      if(lesson.subject && teachers[0] != teachers[1] && teachers[0]){
-        if (lesson.id && lessonPlan.weekNumber == lesson.weekNumber && confirm('Вы действительно хотите обновить пару?') ) {
+      if (lesson.subject && teachers[0] != teachers[1] && teachers[0]) {
+        if (lesson.id && lessonPlan.weekNumber == lesson.weekNumber && confirm('Вы действительно хотите обновить пару?')) {
           await Promise.all([dispatch(lessonPlanActions.put({ ...lesson, teachers: teachers, isDistance: false }))]);
           dispatch(lessonPlanActions.load());
           dispatch(modalsActions.close('lesson'));
-          
+
         }
-        else if(confirm('Вы действительно хотите добавить новую пару?') ){
+        else if (confirm('Вы действительно хотите добавить новую пару?')) {
           await Promise.all([dispatch(lessonPlanActions.post({ ...lesson, teachers: teachers, isDistance: false }))]);
           dispatch(lessonPlanActions.load());
           dispatch(modalsActions.close('lesson'));
@@ -72,77 +74,81 @@ function LessonModal({ lessonPlan, notChangeWeek }) {
       }
     }),
     onDelete: useCallback(() => {
-      if(confirm('Вы действительно хотите удалить пару?')){
-      dispatch(lessonPlanActions.delete(lessonPlan.id));
-      dispatch(modalsActions.close('lesson'));}
+      if (confirm('Вы действительно хотите удалить пару?')) {
+        dispatch(lessonPlanActions.delete(lessonPlan.id));
+        dispatch(modalsActions.close('lesson'));
+      }
     })
   }
 
   return (
-    <ModalLayout labelClose={'Х'} onClose={callbacks.closeModal}
-      title={`${lessonPlan.group.speciality.shortname}-${lessonPlan.group?.name}, ${weekdays[lessonPlan.weekday - 1]}, ${lessonPlan.lessonNumber}-я пара`}>
-      <Spinner active={select.waiting}>
-        <LessonSelect placeholder='Предмет'
-          defaultValue={lessonPlan.subject && lessonPlan.subject.id}
-          onChange={(value) => setLesson({
-            ...lesson, subject: select.subjects.find((subject) => {
-              return subject.id === value
-            })
-          })}
-          selectOptions={select.subjects.map((subject) => {
-            return {
-              value: subject.id,
-              label: subject.name
-            }
-          })} />
-        <LessonSelect placeholder='Основной преподаватель'
-          defaultValue={lessonPlan.teachers && lessonPlan.teachers[0] && lessonPlan.teachers[0].id}
-          onChange={(value) => setTeachers([select.teachers.find((teacher) => {
-            return teacher.id === value
-          }), teachers[1]])}
-          selectOptions={select.teachers.map((teacher) => {
-            return {
-              value: teacher.id,
-              label: `${teacher.surname} ${teacher.name[0]}. ${teacher.patronymic[0]}.`
-            }
-          })} />
-        <LessonSelect placeholder='Запасной преподаватель'
-          defaultValue={lessonPlan.teachers && lessonPlan.teachers[1] && lessonPlan.teachers[1].id}
-          onChange={(value) => setTeachers([teachers[0], select.teachers.find((teacher) => {
-            return teacher.id === value
-          })])}
-          selectOptions={select.teachers.map((teacher) => {
-            return {
-              value: teacher.id,
-              label: `${teacher.surname} ${teacher.name[0]}. ${teacher.patronymic[0]}.`
-            }
-          })} />
-        <LessonSelect placeholder='Кабинет'
-          defaultValue={lessonPlan.audience && lessonPlan.audience.id}
-          onChange={(value) => setLesson({
-            ...lesson, audience: select.audiences.find((audience) => {
-              return audience.id === value
-            })
-          })}
-          selectOptions={select.audiences.map((audience) => {
-            return {
-              value: audience.id,
-              label: audience.number
-            }
-          })} />
-        {!notChangeWeek && <Flex vertical gap='middle' style={{ margin: '15px 0px', alignItems: 'center' }}>
-          <Radio.Group defaultValue={lessonPlan.weekNumber ? lessonPlan.weekNumber : 0}
-            buttonStyle="solid" size="large"
-            onChange={(value) => setLesson({ ...lesson, weekNumber: value.target.value })}
-            options={[{ label: 'Первая неделя', value: 0 }, { label: 'Вторая неделя', value: 1 }]}
-            optionType="button" />
-        </Flex>}
-        <Flex gap='middle' style={{ margin: '15px 0px', justifySelf: 'center' }}>
-          <Button type="primary" size="large" onClick={callbacks.onAccept}>{(lessonPlan.id && lesson.weekNumber == lessonPlan.weekNumber) ? 'Изменить пару' : 'Добавить пару'}</Button>
-          {(lessonPlan.id && lesson.weekNumber == lessonPlan.weekNumber) && <Button type="primary" size="large" danger onClick={callbacks.onDelete}>Удалить пару</Button>}
-        </Flex>
-      </Spinner>
-    </ModalLayout>
+    <>
+      <ToastContainer position="top-center" autoClose={2000}/>
+      <ModalLayout labelClose={'Х'} onClose={callbacks.closeModal}
+        title={`${lessonPlan.group.speciality.shortname}-${lessonPlan.group?.name}, ${weekdays[lessonPlan.weekday - 1]}, ${lessonPlan.lessonNumber}-я пара`}>
+        <Spinner active={select.waiting}>
+          <LessonSelect placeholder='Предмет'
+            defaultValue={lessonPlan.subject && lessonPlan.subject.id}
+            onChange={(value) => setLesson({
+              ...lesson, subject: select.subjects.find((subject) => {
+                return subject.id === value
+              })
+            })}
+            selectOptions={select.subjects.map((subject) => {
+              return {
+                value: subject.id,
+                label: subject.name
+              }
+            })} />
+          <LessonSelect placeholder='Основной преподаватель'
+            defaultValue={lessonPlan.teachers && lessonPlan.teachers[0] && lessonPlan.teachers[0].id}
+            onChange={(value) => setTeachers([select.teachers.find((teacher) => {
+              return teacher.id === value
+            }), teachers[1]])}
+            selectOptions={select.teachers.map((teacher) => {
+              return {
+                value: teacher.id,
+                label: `${teacher.surname} ${teacher.name[0]}. ${teacher.patronymic[0]}.`
+              }
+            })} />
+          <LessonSelect placeholder='Запасной преподаватель'
+            defaultValue={lessonPlan.teachers && lessonPlan.teachers[1] && lessonPlan.teachers[1].id}
+            onChange={(value) => setTeachers([teachers[0], select.teachers.find((teacher) => {
+              return teacher.id === value
+            })])}
+            selectOptions={select.teachers.map((teacher) => {
+              return {
+                value: teacher.id,
+                label: `${teacher.surname} ${teacher.name[0]}. ${teacher.patronymic[0]}.`
+              }
+            })} />
+          <LessonSelect placeholder='Кабинет'
+            defaultValue={lessonPlan.audience && lessonPlan.audience.id}
+            onChange={(value) => setLesson({
+              ...lesson, audience: select.audiences.find((audience) => {
+                return audience.id === value
+              })
+            })}
+            selectOptions={select.audiences.map((audience) => {
+              return {
+                value: audience.id,
+                label: audience.number
+              }
+            })} />
+          {!notChangeWeek && <Flex vertical gap='middle' style={{ margin: '15px 0px', alignItems: 'center' }}>
+            <Radio.Group defaultValue={lessonPlan.weekNumber ? lessonPlan.weekNumber : 0}
+              buttonStyle="solid" size="large"
+              onChange={(value) => setLesson({ ...lesson, weekNumber: value.target.value })}
+              options={[{ label: 'Первая неделя', value: 0 }, { label: 'Вторая неделя', value: 1 }]}
+              optionType="button" />
+          </Flex>}
+          <Flex gap='middle' style={{ margin: '15px 0px', justifySelf: 'center' }}>
+            <Button type="primary" size="large" onClick={callbacks.onAccept}>{(lessonPlan.id && lesson.weekNumber == lessonPlan.weekNumber) ? 'Изменить пару' : 'Добавить пару'}</Button>
+            {(lessonPlan.id && lesson.weekNumber == lessonPlan.weekNumber) && <Button type="primary" size="large" danger onClick={callbacks.onDelete}>Удалить пару</Button>}
+          </Flex>
+        </Spinner>
+      </ModalLayout>
+    </>
   )
 }
 
