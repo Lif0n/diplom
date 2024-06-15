a03da8a74bef641050f5ab8bca2d285c4e4a2f9b
Lif0n <t4sty.c4ke@gmail.com>
2024-06-15 05:12:17 +0400

    fetch fix

--- src/utils/fetch-lesson-plan.js
+++ src/utils/fetch-lesson-plan.js
@@ -1,24 +1,26 @@
 export default function FetchLessonPlan(arr) {
   //проход по всему массиву
   const newArr = arr.map(lp => {
+    console.log(lp);
+    if(lp.teachers.length == 0){
+      return lp;
+    }
     //получение пар, которые идут в одно и тоже время
-    const lpAtSameTime = arr.filter(lp1 => lp1.weekday === lp.weekday && lp1.weeknumber === lp.weeknumber && lp1.lessonNumber === lp.lessonNumber && lp1.id !== lp.id);
+    const lpAtSameTime = arr.filter(lp1 => lp1.weekday === lp.weekday && lp1.weekNumber === lp.weekNumber && lp1.lessonNumber === lp.lessonNumber && lp1.id !== lp.id);
     //массив с ошибками
     let errors = [];
-    //проход по преподам которые ведут текущую пару
-    lp.teachers.forEach(teacher => {
-      //проход по парам, которые идут в это же время
-      lpAtSameTime.forEach(lp1 => {
-        //проход по преподам пары, которая идет  это же время
-        lp1.teachers.forEach(teacher1 => {
-          //проверка на то, одинаковые ли преподы
-          if (teacher.id == teacher1.id) {
-            errors = [...errors, `${teacher.lastName} ${teacher.firstName[0]}. ${teacher.middleName[0]}. уже ведет пару в это время у группы ${lp1.group.groupCode}`];
-          }
-        })
-      })
-    });
+    //проход по парам, которые идут в это же время
+    lpAtSameTime.forEach(lp1 => {
+      //проверка на то, одинаковые ли преподы
+      if(lp1.teachers.length == 0){
+        return lp;
+      }
+      if (lp.teachers[0].id == lp1.teachers[0].id) {
+        errors = [...errors, `${lp.teachers[0].lastName} ${lp.teachers[0].firstName[0]}. ${lp.teachers[0].middleName[0]}. уже ведет пару в это время у группы ${lp1.group.groupCode}`];
+      }
+    })
     if (errors.length > 0) {
+      console.log(errors);
       return { ...lp, errors: errors };
     }
     return lp
